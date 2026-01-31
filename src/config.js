571795f96cd0fdc64c5df1ef2a98d3a858d54f99
Gaurav-AL <reverse454@gmail.com>
2026-01-31 15:32:52 +0530

    changed url

--- src/config.js
+++ src/config.js
@@ -1,9 +1,9 @@
 export const config = {
-  BACKEND_URL: process.env.REACT_APP_BACKEND_URL || 'http://localhost:8080',
+  BACKEND_URL: process.env.REACT_APP_BACKEND_URL || 'https://chat-production-1260.up.railway.app',
   WS_ENDPOINT: '/ws',
   
   getWebSocketUrl: function() {
-    const url = this.BACKEND_URL;
+    const url = this.BACKEND_URL.replace(/^http/, 'ws');
     return `${url}${this.WS_ENDPOINT}`;
   },
   
